c6db762878d45ec89967992f42425d717a539972
rohit568 <hirohit337@gmail.com>
2025-04-28 00:50:12 +0530

    initialised movie system design project

---
+++ src/main/java/com/pack/moviebookingsystem/HiddenprojectApplication.java
@@ -0,0 +1,13 @@
+package com.pack.moviebookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiddenprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiddenprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pack/hiddenproject/HiddenprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pack.hiddenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiddenprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
